e5eb0370ef426d28b3597bfdbc551f5ce11805ac
jonas-fischer <jonas.fischer.42@gmail.com>
2017-05-31 19:35:59 +0200

    f
    git commit -a -m f

--- ttt.py
+++ ttt.py
@@ -12,9 +12,9 @@ import matplotlib as mpl
 mpl.rc("figure", figsize=(12, 6))
 
 T=np.loadtxt('Tundp.txt')[:26]
-chi=np.loadtxt('Tundp.txt')[26:]
+chi=np.loadtxt('Tundp.txt')[26:52]
 
-plt.errorbar(T,chi,yerr=0.01,xerr=0.04,fmt='x',label='gemessene Werte')
+plt.errorbar(T,chi,yerr=0.01,xerr=0.04,fmt='x',label='Messwerte')
 plt.xlabel('T in K')
 plt.ylabel('$\chi$ ~ U in V')
 plt.grid(True)
@@ -27,7 +27,7 @@ def CurieWeiss(beta,T):
     
 data = RealData(T,chi,0.04,0.01)
 model = Model(Curie)
-odr = ODR(data, model, beta0=[0,6])
+odr = ODR(data, model, beta0=[0.686,5.53])
 #odr.set_job(fit_type=2)
 outputC = odr.run()
 paramsC=outputC.beta
@@ -36,7 +36,7 @@ errorC=outputC.sd_beta
 plt.plot(T,Curie(paramsC,T),label='Fit Curie-Gesetz')
 
 model = Model(CurieWeiss)
-odr = ODR(data, model, beta0=[0,6,0])
+odr = ODR(data, model, beta0=[0.184,4.861,0.175])
 #odr.set_job(fit_type=2)
 outputCW= odr.run()
 paramsCW=outputCW.beta
@@ -44,5 +44,55 @@ errorCW=outputCW.sd_beta
 
 plt.plot(T,CurieWeiss(paramsCW,T),label='Fit Curie-Weiss-Gesetz')
 plt.legend(loc='best')
-#plt.savefig('chifit.pdf')
+plt.savefig('chifit.pdf')
 plt.show()
+
+'''
+aufwärmen
+'''
+
+p=np.loadtxt('Tundp.txt')[52:68]
+chi2=np.loadtxt('Tundp.txt')[68:]
+
+def CW(chi):
+    return paramsCW[1]/(chi-paramsCW[0])+paramsCW[2]
+
+T2=CW(chi2)
+plt.errorbar(T2,p,yerr=1,xerr=0.04,fmt='x',label='Messwerte')
+plt.xlabel('T in K')
+plt.ylabel('p in Torr')
+plt.grid(True)
+
+def lin(beta,T):
+    return beta[0]*T+beta[1]
+    
+d=10 #denn Punkt 11 liegt in beiden fits über dem lambda-Punkt
+    
+data = RealData(T2[:d],p[:d],0.04,1)
+model = Model(lin)
+odr = ODR(data, model, beta0=[67.8,-110])
+#odr.set_job(fit_type=2)
+outputL = odr.run()
+paramsL=outputL.beta
+errorL=outputL.sd_beta
+
+l=np.linspace(1.71,2.25)
+
+plt.plot(l,lin(paramsL,l),label='Fit p-T unterhalb $\lambda$-Punkt',zorder=10,color='r')
+
+data = RealData(T2[d:],p[d:],0.04,1)
+model = Model(lin)
+odr = ODR(data, model, beta0=[8183,-17652])
+#odr.set_job(fit_type=2)
+outputL2 = odr.run()
+paramsL2=outputL2.beta
+errorL2=outputL2.sd_beta
+
+plt.plot(l,lin(paramsL2,l),label='Fit p-T oberhalb $\lambda$-Punkt',zorder=10,color='g')
+plt.xlim([1.7,2.3])
+plt.ylim([0,450])
+plt.legend(loc='best')
+plt.savefig('pT.pdf')
+plt.show()
+
+print('T_lambda = ',(paramsL2[1]-paramsL[1])/(paramsL[0]-paramsL2[0]), 'Messpunkt 11: ',T2[10])
